55acae4f5c493cb439c30dba0fd5dc72405b4141
Arif_Kurniawan <kangarif.mas@gmail.com>
2025-03-19 23:45:29 +0700

    style: gradasi background & penambhan skill

--- src/pages/home-page/skills/itss-kills/SkillCard.tsx
+++ src/pages/home-page/skills/itss-kills/SkillCard.tsx
@@ -7,7 +7,7 @@ type SkillProps = {
 
 const SkillCard: React.FC<SkillProps> = ({skill,icon}) => {
     return (
-        <div className="flex flex-col items-center p-5 sm:p-6 bg-white shadow-md rounded-lg hover:shadow-xl transition-transform transform hover:scale-105 duration-300">
+        <div className="flex flex-col items-center p-5 sm:p-6 bg-white shadow-md rounded-3xl hover:shadow-xl transition-transform transform hover:scale-105 duration-300">
             <img src={icon} alt={skill} className="w-16 h-16 md:w-20 md:h-20 mb-3 transition-transform duration-300 hover:rotate-6"/>
             <div className="text-center">
                 <h3 className="text-lg sm:text-xl font-bold">{skill}</h3>
@@ -17,6 +17,7 @@ const SkillCard: React.FC<SkillProps> = ({skill,icon}) => {
 };
 
 const skills: SkillProps[] = [
+    {skill:"HTML", icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpngGRjYX1ca7qAADU3K6eGLj7ShQE3L2otdzfryl_Y9Ht2QRoQKYQbsXd36XIxMbYOw0&usqp=CAU"},
     {skill:"JavaScript", icon:"/Assets/skills/javascript.png"},
     {skill:"TypeScript",  icon:"/Assets/skills/typescript.png"},
     {skill:"TailwindCSS", icon:"/Assets/skills/icons8-tailwindcss-500.png"},
@@ -25,6 +26,7 @@ const skills: SkillProps[] = [
     {skill:"Node JS", icon:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Rfc_ANhyiDRYJEO0hU9lPw_lfadu_nM6rWKRqfx8xLrITFDv68_VkN7-Xoq_yWa9Gw8&usqp=CAU"},
     {skill:"Express JS", icon:"https://mitrainformatikaindonesia.co.id/wp-content/uploads/2024/06/1686391647921.png"},
     {skill:"MySQL", icon:"https://bgasparotto.com/wp-content/uploads/2015/05/mysql-logo.png"},
+    {skill:"GitHub", icon:"https://researchcomputing.princeton.edu/sites/g/files/toruqf7036/files/styles/freeform_750w/public/2021-02/Github.png?h=0adafebd&itok=ifc6IOIP"},
 ];
 
 export default function SkillsSection(){
--- src/pages/home-page/skills/mySkills.tsx
+++ src/pages/home-page/skills/mySkills.tsx
@@ -1,11 +1,12 @@
 import SkillsSection from "./itss-kills/SkillCard"
-
+import React from "react"
+import "@/styles/gradient.css"
 
 
 export default function MySkills() {
 return (
     <>
-    <div className="container mx-auto px-4 bg-white p-[20px] mt-[50px] border rounded-[20px]">
+    <div className="container mx-auto px-4 bg-gradient-to-r from-purple-400 via-indigo-500 to-violet-600 p-[20px] mt-[50px] border rounded-[20px] animate-gradient-flow">
     <h1 className="text-center text-xl font-bold m-[30px]">My Skills</h1>
     <div className="">
         <SkillsSection />
