28a7ccaf59b27ad06ee361fe0890b6df229bf344
Arif_Kurniawan <kangarif.mas@gmail.com>
2025-03-20 00:00:33 +0700

    style

--- src/pages/home-page/skills/mySkills.tsx
+++ src/pages/home-page/skills/mySkills.tsx
@@ -6,7 +6,7 @@ import "@/styles/gradient.css"
 export default function MySkills() {
 return (
     <>
-    <div className="container mx-auto px-4 bg-gradient-to-r from-purple-400 via-indigo-500 to-violet-600 p-[20px] mt-[50px] border rounded-[20px] animate-gradient-flow">
+    <div className="container mx-auto px-2 bg-gradient-to-r from-purple-400 via-indigo-500 to-violet-600 p-[20px] mt-[50px] border rounded-[20px] animate-gradient-flow">
     <h1 className="text-center text-xl font-bold m-[30px]">My Skills</h1>
     <div className="">
         <SkillsSection />
